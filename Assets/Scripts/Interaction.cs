28d7c6b8ef785a97441d39df73baec6da1a57f20
Ishan Kamboj <ISHANKAMBOJ35@GMAIL.COM>
2024-12-01 16:56:52 +0530

    NPC interactions added

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interaction : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject dialoguePanel;
+    [SerializeField]
+    public TextMeshProUGUI dialogueText;
+    [SerializeField]
+    public string[] dialogue;
+    [SerializeField]
+    public float wordSpeed;
+    [SerializeField]
+    private bool playerClose;
+
+    private int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialoguePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerClose && Input.GetKeyDown(KeyCode.E))
+        {
+            if(dialoguePanel.activeInHierarchy)
+            {
+                zeroText();
+            }
+            else
+            {
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
+        }
+        if(dialogueText.text == dialogue[index])
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                NextLine();
+            }
+        }
+        
+    }
+
+    public void zeroText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+    }
+    public void NextLine()
+    {
+        if(index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            zeroText();
+        }
+    }
+
+    IEnumerator Typing()
+    {
+        foreach( char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerClose = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerClose = false;
+            zeroText();
+        }
+    }
+}
