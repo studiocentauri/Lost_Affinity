6799e16247a5446a0aec479b5084877b3407c137
Priyank Agnihotri <priyankagnihotri1@gmail.com>
2024-12-01 23:45:04 +0530

    updated checkpt
    checkpoint set to last acquired checkpt

--- Assets/Scripts/checkpoint.cs
+++ Assets/Scripts/checkpoint.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class checkpoint : MonoBehaviour
 {
     GameController_chkpt gameController;
+    Collider2D coll;
 
     private void Awake()
     {
         gameController = GameObject.FindGameObjectWithTag("Player").GetComponent<GameController_chkpt>();
+        coll = GetComponent<Collider2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -16,6 +18,7 @@ public class checkpoint : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             gameController.UpdateCheckpoint(transform.position);
+            coll.enabled = false;
         }
     }
 }
