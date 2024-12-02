6a739ce278569857a351a1324b1071197a3c9af6
Ishan Kamboj <ISHANKAMBOJ35@GMAIL.COM>
2024-12-02 23:20:57 +0530

    level 1 done

---
+++ Assets/Scripts/Platforms/PlatformOscillation.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlatformOscillation : MonoBehaviour
+{
+    private Vector3 startPoint; 
+    private Vector3 endPoint; 
+    public float speed = 2f;
+    public Vector3 endPointCoords;
+    private Vector3 targetPosition;
+    public bool movementIsTriggered;
+    void Start()
+    {
+        startPoint = transform.position;
+        endPoint = transform.position;
+        SetStartPoint();
+        SetEndPoint();
+        targetPosition = endPoint;
+    }
+    void SetStartPoint()
+    {
+        startPoint = transform.position;
+    }
+    void SetEndPoint()
+    {
+        endPoint = transform.position + endPointCoords;
+    }
+    void Update()
+    {
+        if(movementIsTriggered)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+            if (Vector2.Distance(transform.position, targetPosition) < 0.1f)
+            {
+                targetPosition = (targetPosition == startPoint) ? endPoint : startPoint;
+            }
+        }
+        
+    }
+}
