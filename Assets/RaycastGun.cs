44035813fb9d484ef7c081cf21ec6363d4d42931
Anantag24 <anantag23@iitk.ac.in>
2024-12-03 00:32:56 +0530

    yugyu

---
+++ Assets/RaycastGun.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class LaserTut : MonoBehaviour {
+    public Camera cam;
+    public LineRenderer lineRenderer;
+    public Transform firePoint;
+    public GameObject startVFX;
+    public GameObject endVFX;
+
+}
