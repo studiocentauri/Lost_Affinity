c0a1f24bfc74f8a105a1f9fae6aee1180733f994
krishag23 <sgc.krish.8mar.10sci@gmail.com>
2024-12-03 23:52:25 +0530

    UI NPC Tutorial

---
+++ Assets/Scripts/UITutorial.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITutorial : MonoBehaviour
+{
+    public GameObject promptText;
+    private bool isPlayerNearby = false;
+
+    void Start()
+    {
+        if (promptText != null)
+        {
+            promptText.SetActive(false); // Ensure the text is hidden initially
+        }
+        else
+        {
+            Debug.LogWarning("PromptText is not assigned!");
+        }
+    }
+
+    void Update()
+    {
+        if (isPlayerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            // Hide the text after pressing E
+            if (promptText != null)
+            {
+                promptText.SetActive(false);
+            }
+
+
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNearby = true;
+
+            if (promptText != null)
+            {
+                promptText.SetActive(true);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) // Check if the object exiting the trigger is the player
+        {
+            isPlayerNearby = false;
+
+            if (promptText != null)
+            {
+                promptText.SetActive(false); // Hide the "Press E to Talk" text
+            }
+        }
+    }
+}
