15b13fca37acc4fc59ef494a86c69df981f4f27a
AryanVerma212 <aryanverma212@outlook.com>
2024-12-07 00:12:00 +0530

    (Optional) Added Username Script and some scripts
    Improvements needed-----

---
+++ Assets/Scripts/Buddha_Interaction.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Buddha_Interaction : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject dialoguePanel;
+    [SerializeField]
+    public TextMeshProUGUI dialogueText;
+    [SerializeField]
+    public string[] dialogue;
+    public Texture[] speakerImage;
+    public string[] speaker;
+    [SerializeField]
+    public float wordSpeed;
+    [SerializeField]
+    private bool playerClose;
+    public RawImage rawImage;
+    public TMP_Text speakerName;
+
+    private int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialoguePanel.SetActive(false);
+        ReplaceName();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerClose && Input.GetKeyDown(KeyCode.E))
+        {
+            if (dialoguePanel.activeInHierarchy)
+            {
+                NextLine();
+            }
+            else
+            {
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
+        }
+        if (dialogueText.text == dialogue[index])
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //NextLine();
+            }
+        }
+
+    }
+
+    public void zeroText()
+    {
+        StopAllCoroutines();
+        dialogueText.text = "";
+        index = 0;
+
+        dialoguePanel.SetActive(false);
+    }
+    public void NextLine()
+    {
+        if (index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            zeroText();
+        }
+    }
+
+    IEnumerator Typing()
+    {
+        //istyping = true;
+        rawImage.texture = speakerImage[index];
+        speakerName.text = speaker[index];
+        int i = index;
+        foreach (char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+            if (i != index)
+            {
+                break;
+            }
+        }
+
+        //istyping = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerClose = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerClose = false;
+            zeroText();
+        }
+    }
+    private void ReplaceName()
+    {
+        for (int i = 0; i < dialogue.Length; i++)
+        {
+            dialogue[i] = dialogue[i].Replace("player", GameManager.Instance.PlayerName);
+        }
+    }
+}
