85d344aee8c6845ab69fcbdf2f33fff5ebe136f4
Kunal Shikhar <kunalshikhar2@gmail.com>
2024-12-07 02:44:48 +0530

    Game Ending Added

---
+++ Assets/Scripts/Ending.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ending : MonoBehaviour
+{
+    public Animator animator;
+    public activateAutoPlayerMove check;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && check.CanEnd)
+        {
+                animator.SetBool("GateOpen", true);
+                Invoke("SceneChange", 0.3f);
+                collision.GetComponent<playermovement>().enabled = false;
+                collision.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                collision.GetComponentInChildren<Animator>().enabled = false;
+        }
+    }
+
+    void SceneChange()
+    {
+        //To add Ending Cutscene Scene
+    }
+}
