16c53ac6e2320aeb5cb4bb2c7dc9d53f896b6283
Ishan Kamboj <ISHANKAMBOJ35@GMAIL.COM>
2024-12-07 03:25:58 +0530

    camera shake added

--- Assets/activateAutoPlayerMove.cs
+++ Assets/activateAutoPlayerMove.cs
@@ -1,19 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+using System.Threading;
 
 public class activateAutoPlayerMove : MonoBehaviour
 {
     // Start is called before the first frame update
     public InventoryManager inventoryManager;
+    public CinemachineVirtualCamera _cinemachineVirtualCamera;
+    public float shakeIntensity = 1.0f;
+    public float shakeTime = 0.5f;
+    private float time = 0;
+
+    private CinemachineBasicMultiChannelPerlin _cbmcp;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(inventoryManager.redSlot != null && inventoryManager.blueSlot != null && inventoryManager.greenSlot != null)
         {
+            ShakeCamera();
             AutoMovePlayerOnEnd _autoMovePlayerOnEnd = collision.gameObject.GetComponent<AutoMovePlayerOnEnd>();
             _autoMovePlayerOnEnd.enabled = true;
+            
             /*ControlAutoMoveAnimation controlAutoMoveAnimation = collision.gameObject.GetComponent<ControlAutoMoveAnimation>();
             controlAutoMoveAnimation.enabled = true;*/
         }
     }
+    public void ShakeCamera()
+    {
+        CinemachineBasicMultiChannelPerlin _cbmcp = _cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        _cbmcp.m_AmplitudeGain = shakeIntensity;
+        _cbmcp.m_FrequencyGain = shakeIntensity;
+        Debug.Log("shake");
+        time = shakeTime;
+
+    }
+    public void StopShake()
+    {
+        CinemachineBasicMultiChannelPerlin _cbmcp = _cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        _cbmcp.m_AmplitudeGain = 0;
+        _cbmcp.m_FrequencyGain = 0;
+        time = 0;
+    }
+    private void Update()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+            if(time <= 0)
+            {
+                StopShake();
+            }
+        }
+    }
+
 }
