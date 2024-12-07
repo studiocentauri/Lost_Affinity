88bc3cb315210922a7df3aca3d1628582fbc6e88
mdarsal23 <mdarsal23@iitk.ac.in>
2024-12-07 20:12:09 +0530

    Warning fixed

--- Assets/Scripts/Warning _ Artifact.cs
+++ Assets/Scripts/Warning _ Artifact.cs
@@ -13,7 +13,7 @@ public class Warning_Artifact : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Alpha1))
+        if(Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Alpha5) || Input.GetKeyDown(KeyCode.Alpha6) || Input.GetKeyDown(KeyCode.Alpha7) || Input.GetKeyDown(KeyCode.Alpha8) || Input.GetKeyDown(KeyCode.Alpha9) || Input.GetKeyDown(KeyCode.Alpha0))
         {
             Warning.SetActive(true);
             Invoke("HideWarning", 3f);
